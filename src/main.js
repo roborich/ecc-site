ebe4d9e763d10df31489d54ad526d6179f699992
roborich <rich@specialtycommunicationsinc.com>
2019-02-24 22:18:53 -0500

    added netlify identity widget

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
-
+import netlifyIdentity from 'netlify-identity-widget';
 import DefaultLayout from "~/layouts/Default.vue";
 import registerIcons from "./registerIcons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
+netlifyIdentity.init();
 registerIcons();
 
 export default function(Vue, { router, head, isClient }) {
