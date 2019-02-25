14a9fd6f5471790c3a3fb6450feb2e48741e7121
roborich <rich@specialtycommunicationsinc.com>
2019-02-24 21:50:22 -0500

    add cms

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from "~/layouts/Default.vue";
-import registerIcons from './registerIcons';
+import registerIcons from "./registerIcons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 registerIcons();
